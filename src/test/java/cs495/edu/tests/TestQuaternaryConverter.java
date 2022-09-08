570fcc077622c2389e4a2d93dce51aaf858a4401
Tyler Newlin <tnewlin25@gmail.com>
2022-09-08 14:36:13 -0400

    Make unit test for toDecimal method paramterized for more test cases

--- src/test/java/cs495/edu/tests/TestQuaternaryConverter.java
+++ src/test/java/cs495/edu/tests/TestQuaternaryConverter.java
@@ -2,11 +2,18 @@ package cs495.edu.tests;
 
 import cs495.edu.quaternarycalculator.QuaternaryConverter;
 import org.junit.jupiter.api.Assertions;
-import org.junit.jupiter.api.Test;
+import org.junit.jupiter.params.ParameterizedTest;
+import org.junit.jupiter.params.provider.CsvSource;
 
 public class TestQuaternaryConverter {
-    @Test
-    public void testToDecimal() {
-        Assertions.assertEquals(210, QuaternaryConverter.toDecimal("3102"));
+    @ParameterizedTest
+    @CsvSource(delimiter = '|', value = {
+            "210 | 3102",
+            "846 | 31032",
+            "121 | 1321",
+            "20077 | 10321231"
+    })
+    public void testToDecimal(int expected, String quaternary) {
+        Assertions.assertEquals(expected, QuaternaryConverter.toDecimal(quaternary));
     }
 }
