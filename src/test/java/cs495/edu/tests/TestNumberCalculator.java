f6763d69685da0d42c0a6022fdd870bb56651bf5
NoahPerz <nprez32@gmail.com>
2022-09-09 15:47:53 -0400

    Completing tests for subtraction operation

--- src/test/java/cs495/edu/tests/TestNumberCalculator.java
+++ src/test/java/cs495/edu/tests/TestNumberCalculator.java
@@ -3,11 +3,27 @@ package cs495.edu.tests;
 import cs495.edu.quaternarycalculator.NumberCalculator;
 import org.junit.jupiter.api.Assertions;
 import org.junit.jupiter.api.Test;
+import org.junit.jupiter.params.ParameterizedTest;
+import org.junit.jupiter.params.provider.CsvSource;
 
 public class TestNumberCalculator {
     NumberCalculator numberCalculator = new NumberCalculator();
-    @Test
-    public void subOperatorToTest(){
-        Assertions.assertEquals(2,numberCalculator.subOperation("3", "1"));
+
+    @ParameterizedTest
+    @CsvSource(value = {
+            "2,3,1",
+            "0,1,2",
+            "130,302,112",
+            "110,1123,1013"
+
+    })
+    public void subtractTest(String expected, String numberSubtracted, String numberToSubtract){
+        Assertions.assertEquals(expected,numberCalculator.subtract(numberSubtracted, numberToSubtract));
     }
+
+
+
+
+
+
 }
