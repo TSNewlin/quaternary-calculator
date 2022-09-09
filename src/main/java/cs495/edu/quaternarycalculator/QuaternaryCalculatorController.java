4d8b349954239f3dba4eca172216a0fbcd6e6042
Tyler Newlin <tnewlin25@gmail.com>
2022-09-08 22:35:52 -0400

    Add rudimentary functionality for operation buttons and equals button

--- src/main/java/cs495/edu/quaternarycalculator/QuaternaryCalculatorController.java
+++ src/main/java/cs495/edu/quaternarycalculator/QuaternaryCalculatorController.java
@@ -7,14 +7,55 @@ import javafx.event.ActionEvent;
 
 public class QuaternaryCalculatorController {
     private String displayedQuaternary = "";
+    private String prevQuaternary = "";
+    private String operation = "";
+    private boolean newOperation = true;
 
     @FXML
     private TextField numberDisplay;
+    @FXML
+    private Button addition;
+    @FXML
+    private Button subtraction;
+    @FXML
+    private Button multiplication;
+    @FXML
+    private Button division;
 
     @FXML
     protected void onNumberButtonCLick(ActionEvent event) {
         Button buttonPressed = (Button) event.getSource();
+        if (newOperation) {
+            numberDisplay.setText("");
+            displayedQuaternary = "";
+            newOperation = false;
+        }
         displayedQuaternary += buttonPressed.getText();
         numberDisplay.setText(displayedQuaternary);
+    }
+
+    @FXML
+    protected void onOperationButtonClick(ActionEvent event) {
+        Button operationButton = (Button) event.getSource();
+        operation = operationButton.getText();
+        prevQuaternary = displayedQuaternary;
+        displayedQuaternary = "";
+        numberDisplay.setText("");
+        addition.setDisable(true);
+        subtraction.setDisable(true);
+        multiplication.setDisable(true);
+        division.setDisable(true);
+    }
+
+    @FXML
+    protected void onEqualButtonClick() {
+        newOperation = true;
+        numberDisplay.setText(prevQuaternary + " " + operation + " " + displayedQuaternary);
+        prevQuaternary = "";
+        displayedQuaternary = "";
+        addition.setDisable(false);
+        subtraction.setDisable(false);
+        multiplication.setDisable(false);
+        division.setDisable(false);
     }
 }
