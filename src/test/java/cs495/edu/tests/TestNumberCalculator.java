7c068fa5fe04722cc7ff1859136918f8afd49ac7
NoahPerz <nprez32@gmail.com>
2022-09-09 15:00:01 -0400

    Structuring the subtraction operation along with the start of a test

--- src/test/java/cs495/edu/tests/TestNumberCalculator.java
+++ src/test/java/cs495/edu/tests/TestNumberCalculator.java
@@ -1,6 +1,13 @@
 package cs495.edu.tests;
 
+import cs495.edu.quaternarycalculator.NumberCalculator;
+import org.junit.jupiter.api.Assertions;
 import org.junit.jupiter.api.Test;
 
 public class TestNumberCalculator {
+    NumberCalculator numberCalculator = new NumberCalculator();
+    @Test
+    public void subOperatorToTest(){
+        Assertions.assertEquals(2,numberCalculator.subOperation("3", "1"));
+    }
 }
