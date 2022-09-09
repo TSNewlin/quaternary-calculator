677e0cb3ae69e6e3b97df400a456f5603af78ccb
Tyler Newlin <tnewlin25@gmail.com>
2022-09-09 15:15:54 -0400

    Change default window size

--- src/main/java/cs495/edu/quaternarycalculator/QuaternaryCalculator.java
+++ src/main/java/cs495/edu/quaternarycalculator/QuaternaryCalculator.java
@@ -11,7 +11,7 @@ public class QuaternaryCalculator extends Application {
     @Override
     public void start(Stage stage) throws IOException {
         FXMLLoader fxmlLoader = new FXMLLoader(QuaternaryCalculator.class.getResource("calculator-view.fxml"));
-        Scene scene = new Scene(fxmlLoader.load(), 320, 240);
+        Scene scene = new Scene(fxmlLoader.load(), 310, 425);
         stage.setTitle("Hello!");
         stage.setScene(scene);
         stage.show();
