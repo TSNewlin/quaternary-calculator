190c3e917d6cac06604dfdf82d9b657b7bbef6d6
NoahPerz <nprez32@gmail.com>
2022-09-09 15:00:01 -0400

    Structuring the subtraction operation along with the start of a test

--- src/main/java/cs495/edu/quaternarycalculator/NumberCalculator.java
+++ src/main/java/cs495/edu/quaternarycalculator/NumberCalculator.java
@@ -1,5 +1,18 @@
 package cs495.edu.quaternarycalculator;
 
-public class NumberCalculator {
+import java.util.Scanner;
 
+public class NumberCalculator {
+    public String subOperation(String quaternary1, String quaternary2){
+        int num1 = Integer.parseInt(quaternary1, 4);
+        int num2 = Integer.parseInt(quaternary2, 4);
+        int finalTotal = num1 - num2;
+
+        if (finalTotal < 0){
+            finalTotal = -finalTotal;
+            return "-" + Integer.toString(finalTotal, 4);
+        }else{
+            return Integer.toString(finalTotal, 4);
+        }
+    }
 }
